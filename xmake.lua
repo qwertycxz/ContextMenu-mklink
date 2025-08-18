fbb0f3914a397f3845e99b2ff3c550425ba34b8b
qwertycxz <gmail2@qq.com>
2025-08-18 09:28:55 +0800

    chore: xmake clean

--- xmake.lua
+++ xmake.lua
@@ -4,22 +4,6 @@ add_includedirs'C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/cppw
 add_rules'mode.release'
 add_syslinks('ole32', 'oleaut32', 'runtimeobject', 'shlwapi')
 add_vectorexts'all'
-after_build(function(target)
-	import("detect.sdks.find_mingw")
-	local mingw_dir = find_mingw().bindir
-	local target_dir = target:targetdir()
-	local function link(file)
-		try {
-			function ()
-				os.ln(mingw_dir .. '/' .. file, target_dir .. '/' .. file)
-			end
-		}
-	end
-	link'libgcc_s_seh-1.dll'
-	link'libstdc++-6.dll'
-	link'libwinpthread-1.dll'
-	os.runv('cim -h 1000 -w 1000 png src/Logo.svg', target_dir)
-end)
 on_load(function(target)
 	local version, commit = os.iorun'git describe --match v* --tags':match'^v(%d+%.%d+%.%d+)%-?(%d*)'
 	if commit == '' then
@@ -37,8 +21,29 @@ set_pcxxheader'src/pch.hpp'
 set_project'ContextMenu-mklink'
 set_toolchains'mingw'
 set_warnings('everything', 'pedantic')
-target'x64'
 
+after_build(function(target)
+	import("detect.sdks.find_mingw")
+	local mingw_dir = find_mingw().bindir
+	local target_dir = target:targetdir()
+	local function link(file)
+		try {
+			function ()
+				os.ln(mingw_dir .. '/' .. file, target_dir .. '/' .. file)
+			end
+		}
+	end
+	link'libgcc_s_seh-1.dll'
+	link'libstdc++-6.dll'
+	link'libwinpthread-1.dll'
+	os.runv('cim -h 1000 -w 1000 png src/Logo.svg', target_dir)
+end)
+
+after_clean(function(target)
+	os.rm(target:targetdir())
+end)
+
+target'x64'
 local function format()
 	return table.join(os.files'src/**', os.files'**/*.json')
 end
